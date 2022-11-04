f8dfbfb0193e13e78fb98b8ed46fb19c911b9280
shaked777 <shaked.g1010@gmail.com>
2022-11-04 19:03:56 +0200

    city populate

--- main/management/commands/city.py
+++ main/management/commands/city.py
@@ -1,15 +1,15 @@
 from django.core.management.base import BaseCommand
 import pandas as pd
 from main.models import Trip
-
+import os
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-
+        cwd = os.getcwd()
         # Process data with Pandas
-        data = pd.read_csv("/Users/shakedgabay/Desktop/python/python_course/reccomandition_system/world_cities.csv")
+        data = pd.read_csv(f"{cwd}/world_cities.csv")
         data = data.rename(columns={'description ': 'description'})
 
         # iterate over DataFrame and create your objects
